cd0d33b4ad8df701be96ae8632933a316916c8a8
Wesleynicodemos <Wesleyn1373@hotmail.com>
2024-07-12 11:04:43 -0300

    Adição de responsividade e botao mobile
    Adição de responsividade para tela de tablets e botão de menu para telas mobile.

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ const observador = new IntersectionObserver ( (entradas) => {
 const elementos = document.querySelectorAll('.hidden ,.hidden-2')
 
 elementos.forEach( (elemento) => observador.observe(elemento))
+
+/*Animação do botão*/
+
+const btnmenu = document.querySelector('.btn-menu')
+
+function ativabtn() {
+    const ativa = document.querySelector('.nav')
+    const ativaicon = document.querySelector('.btn-menu')
+    ativa.classList.toggle ('ativo')
+    ativaicon.classList.toggle ('ativo')
+
+    
+
+}
+
+btnmenu.addEventListener('click', ativabtn)
+
